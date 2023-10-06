03bb4c701f32f874fe7fd02ab92a50505d0682e4
Thiago Ferreira <thiago.ferreira@dqrtech.dev.br>
2023-10-05 22:14:43 -0300

    feat: add main application class

---
+++ src/main/java/com/pismo/Application.java
@@ -0,0 +1,14 @@
+package com.pismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.pismo")
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}

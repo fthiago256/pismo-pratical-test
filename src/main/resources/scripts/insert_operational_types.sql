e7ed775388ad4b838ffd1fba1f4f8cd37a68d82a
Thiago Ferreira <thiago.ferreira@dqrtech.dev.br>
2023-10-05 22:13:51 -0300

    chore: add properties and scripts resources

---
+++ src/main/resources/scripts/insert_operational_types.sql
@@ -0,0 +1,4 @@
+insert into operational_type (id, description) values (1, 'CASH PURCHASE');
+insert into operational_type (id, description) values (2, 'INSTALLMENT PURCHASE');
+insert into operational_type (id, description) values (3, 'WITHDRAWAL');
+insert into operational_type (id, description) values (4, 'PAYMENT');

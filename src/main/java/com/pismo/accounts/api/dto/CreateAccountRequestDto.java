d84a189c90595ea9eeffdbdb62588d5d11482576
Thiago Ferreira <thiago.ferreira@dqrtech.dev.br>
2023-10-05 23:37:01 -0300

    change: fix json property mapping on dtos

--- src/main/java/com/pismo/accounts/api/dto/CreateAccountRequestDto.java
+++ src/main/java/com/pismo/accounts/api/dto/CreateAccountRequestDto.java
@@ -1,9 +1,11 @@
 package com.pismo.accounts.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pismo.accounts.Account;
 
 public class CreateAccountRequestDto {
 
+    @JsonProperty("document_number")
     public String documentNumber;
 
     public Account toAccount() {
--- src/main/java/com/pismo/transactions/api/dto/CreateTransactionRequestDto.java
+++ src/main/java/com/pismo/transactions/api/dto/CreateTransactionRequestDto.java
@@ -1,13 +1,17 @@
 package com.pismo.transactions.api.dto;
 
-import com.pismo.accounts.Account;
-import com.pismo.transactions.Transaction;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class CreateTransactionRequestDto {
 
+    @JsonProperty("account_id")
     public Long accountId;
 
+
+    @JsonProperty("operation_type_id")
     public Long operationalTypeId;
 
+    @JsonProperty("amount")
     public Double amount;
 }
